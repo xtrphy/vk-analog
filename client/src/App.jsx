eccd1b19cef6971926a0cb6e6fb2b4db8e49676f
xtrphy <perepeckinvlad@gmail.com>
2025-05-19 16:50:22 +0500

    Create header, set up layout, install dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header/Header"
+import Feed from "./pages/Feed/Feed"
+
+function App() {
+
+    return (
+        <>
+            <Header />
+            <Feed />
+        </>
+    )
+}
+
+export default App;

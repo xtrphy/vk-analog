55787055f7c40fd48cc711378853b7ffcb5cd154
xtrphy <perepeckinvlad@gmail.com>
2025-06-01 22:39:35 +0500

    cors

--- server/app.js
+++ server/app.js
@@ -10,35 +10,9 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: function (origin, callback) {
-        const allowedOrigins = [
-            process.env.CLIENT_URL,
-            'http://localhost:5173',
-            'https://vkonnekte-app.netlify.app'
-        ].filter(Boolean);
-
-        console.log('Request origin:', origin);
-        console.log('CLIENT_URL from env:', process.env.CLIENT_URL);
-        console.log('Allowed origins:', allowedOrigins);
-        console.log('NODE_ENV:', process.env.NODE_ENV);
-
-        if (!origin) return callback(null, true);
-
-        if (allowedOrigins.indexOf(origin) !== -1) {
-            console.log('CORS allowed for origin:', origin);
-            callback(null, true);
-        } else {
-            console.log('CORS blocked origin:', origin);
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: process.env.CLIENT_URL,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-    exposedHeaders: ['Set-Cookie']
 }));
-
-app.options('*', cors());
 
 app.use(
     session({
